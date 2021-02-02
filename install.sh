be07c99b9b292c36c5975cde1e107bc20486e4af
Brandon Page <bpage1@salesforce.com>
2021-02-02 14:26:55 -0800

    Fix Carthage builds.

--- install.sh
+++ install.sh
@@ -5,15 +5,17 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     sudo apt-get install libqt5widgets5
     sudo npm install -g cordova@10.0.0
     sudo cordova telemetry off
+    sudo npm install -g typescript
     gem install --no-document fastlane
 else
     npm install -g cordova@10.0.0
     cordova telemetry off
     sudo gem install cocoapods
     sudo npm install plist
-    sudo chown -R $USER ~/.config
+    sudo gem install fastlane
+    sudo npm install -g typescript
+    sudo gem install --no-document fastlane
 fi
 
 git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
 cd SalesforceMobileSDK-Package && sudo node ./install.js
-sudo gem install fastlane
