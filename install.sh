c984613433f7213f66353263df3a2587a580910d
Brandon Page <bpage1@salesforce.com>
2021-11-29 14:55:33 -0800

    no message

--- install.sh
+++ install.sh
@@ -15,6 +15,7 @@ else
     sudo gem install fastlane
     sudo npm install -g typescript
     sudo gem install --no-document fastlane
+    sudo chown -R $USER:$GROUP /Users/distiller/.config/yarn
 fi
 
 git clone --branch dev --single-branch --depth 1 https://github.com/forcedotcom/SalesforceMobileSDK-Package.git
