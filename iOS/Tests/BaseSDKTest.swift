f485a93f3a94d9e0566e437c8948d71d6552946c
Brandon Page <bpage@salesforce.com>
2026-04-09 16:02:40 -0700

    Workaround API 35 login page issue.  Update ReactNative Page Objects for new UI.

--- iOS/Tests/BaseSDKTest.swift
+++ iOS/Tests/BaseSDKTest.swift
@@ -83,9 +83,12 @@ class BaseSDKTest: XCTestCase {
                 verifyInWebView(app: app, text: "Marc Benioff")
             }
         case .reactNative:
-            XCTAssert(app.descendants(matching: .textField).firstMatch.waitForNonExistence(timeout: timeout * 2), appLoadError)
-            let titlePredicate = NSPredicate(format: "label CONTAINS[c] %@", sampleAppTitle)
-            XCTAssert(app.descendants(matching: .any).matching(titlePredicate).firstMatch.waitForExistence(timeout: timeout * 2), appLoadError)
+            let rnTitle = app.staticTexts["Contacts"].firstMatch
+            XCTAssert(rnTitle.waitForExistence(timeout: timeout * 2), appLoadError)
+
+            // Check contacts loaded
+            let rnContact = app.staticTexts["Marc Benioff"].firstMatch
+            XCTAssert(rnContact.waitForExistence(timeout: timeout), mobileSyncError)
         case .mobileSyncSwift:
             let title = app.navigationBars["Contacts"].staticTexts["Contacts"]
             XCTAssert(title.waitForExistence(timeout: timeout), appLoadError)
